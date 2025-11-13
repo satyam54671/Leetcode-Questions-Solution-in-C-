defc3aab54e7720f6e5ad91f73255286c8a7f4b2
Satyam <satyamraj54671@gmail.com>
2025-11-13 11:13:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.07%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    bool isAnagram(std::string s, std::string t) {
+        std::unordered_map<char, int> ma;
+        std::unordered_map<char, int> mb;
+        
+        for(const auto &el: s)
+        {
+            ma[el]++;
+        }
+        
+        for(const auto &el: t)
+        {
+            mb[el]++;
+        }
+        
+        return ma == mb; 
+    }
+};
